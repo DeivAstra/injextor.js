942a67af3f1754bdf6871e99cbd71e79b3f836eb
danykey <danykey@gmail.com>
2021-07-31 13:56:38 +0300

    Update injextor.js

--- injextor.js
+++ injextor.js
@@ -15,77 +15,71 @@
 				throw 'Context is not defined';
 
 			if (!attrs)
-				throw 'Attributes array or value is not defined';
+				throw 'Attributes names array or attribute name are(is) not defined';
 
 			if (!Array.isArray(attrs) && typeof attrs != 'string')
-				throw 'Expected attr names array or attr name';
+				throw 'Expected attributes names array or attribute name string';
 
+			const names = [];
 			if (Array.isArray(attrs)) {
 				attrs.forEach(function (attr, i) {
-					injectOne(this, context, attr);
+					names.push.apply(names, injectOne(this, context, attr));
 				});
 			} else {
-				injectOne(this, context, attrs);
+				names.push.apply(names, injectOne(this, context, attrs));
 			}
-
-			context.applyValues = (function (values) {
-				var context = this;
-				try {
-					for(prop in values) {
-						
-						if (!context.hasOwnProperty(prop))
-							throw 'Property not found in context:' + prop;
-
-						if (context[prop] instanceof jQuery)
-							context[prop].val(values[prop]);
-						else if (typeof context[prop] != 'function')
-							console.warn('injextor: Object in context in property = '+ prop +' is not jQuery object');
-					}
-				} catch (e) {
-					console.error(e);
-					if (alertOnError)
-						alert(e);
-				}
-			}).bind(context);
-
+			context.applyValues = (applyValues).bind(context);
+			console.log('injext: ' + names.join(', '));
 		} catch (e) {
 			console.error(e);
 			if (alertOnError)
 				alert(e);
 		}
 		return this;
-	}
+	};
 
-	function injectOne(root, context, attr) {
+	function injectOne(section, context, attr) {
 
-		var $root;
-		if (root instanceof jQuery)
-			$root = root;
-		else if (typeof root === 'string')
-			$root = $(root);
+		var $section;
+		if (section instanceof jQuery)
+			$section = section;
+		else if (typeof section === 'string')
+			$section = $(section);
 		else
 			throw 'Unexpected root type. Allowed jQuery object or selector string only';
 
-		if (!$root.hasAttr(attr)) {
-			$root.attr(attr, 'root');
-		}
-
-		function check(name) {
-			if (!name || name.trim() === '')
-				throw 'Attribute defined but empty';
-			if (context.hasOwnProperty(name)) {
-				throw 'Element with attribute "' + attr + ' = '
-						+ name + '" already injected';
+		function check(attrValue) {
+			if (!attrValue || attrValue.trim() === '')
+				throw 'Attribute defined but value is empty';
+			if (context.hasOwnProperty(attrValue)) {
+				throw 'Attribute "' + attr + ' = '
+						+ attrValue + '" already injected';
 			}
 		}
 
-		$root.find('*[' + attr + ']').add($root).each(function (idx, el) {
-			var $el = $(el);
-			var name = $el.attr(attr);
-			check(name);
-			context[name] = $el;
-			console.log(String(idx + 1) + ': ' + name + ' injected');
+		const names = [];
+		$section.find('*[' + attr + ']').each(function (idx, el) {
+			const $el = $(el);
+			const attrValue = $el.attr(attr);
+			check(attrValue);
+			context[attrValue] = $el;
+			names.push(attrValue);
 		});
+		return names;
+	};
+
+	function applyValues (values) {
+		const context = this;
+		for(prop in values) {
+
+			if (!context.hasOwnProperty(prop))
+				throw 'Property not exists in context:' + prop;
+
+			if (context[prop] instanceof jQuery)
+				context[prop].val(values[prop]);
+			else if (typeof context[prop] != 'function')
+				console.warn('Object in context in property = '+ prop +' is not jQuery object');
+		}
 	}
 
 }(jQuery));
